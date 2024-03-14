e8518e37799f1f249c6514d79ccff9e694fa2b44
oscarmmcl <845716@unizar.es>
2024-03-14 16:18:27 +0100

    alex es un pesado

--- head.h
+++ head.h
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int L;
+#define L 10
+
 char s[L*L];
--- observables.c
+++ observables.c
@@ -1,5 +1,10 @@
 #include "head.h"
 
+double magnetizacion (char *s){
+    int i;
+    double r;
+    for (i=0; i<L*L; i++)
 
+}
 
 
