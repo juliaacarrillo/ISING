e7a14da23d7ede03c5b856185fbd322da48fc0cb
Julia Carrillo <850440@unizar.es>
2024-03-24 18:39:18 +0100

    generador creado para rellenar

--- main.c
+++ main.c
@@ -4,14 +4,11 @@ int main()
 {
     int xp[L],xn[L],yp[L],yn[L];
     int s[V];
-    char s_inicial;
-    char s_final;
-    int E_i;
-    int E_f;
-    int n;
-    double beta;
-    printf("Hola mundo");
-    int Naceptados;
-    int Ind;
+    double e;
+    up_config(s);
+    saveConfiguration(s);
+    toro(xp,xn,yp,yn);
+    e=energy(s,xp,yp);
+    printf("%f\n",e);
     return 0;
 }
