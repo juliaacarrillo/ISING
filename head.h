3ed395ffe25aa203b1eb5f2deb525e2d0d7b3406
Ruben Orleans <821151@unizar.es>
2024-03-26 12:03:33 +0100

    nashe

--- head.h
+++ head.h
@@ -2,11 +2,45 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-#define Frec_Max 100
-#define NormRANu (2.3283063671E-10F)
 #define fran (rand()/((double)RAND_MAX+1))
 #define L 10
 #define V L*L 
+
+typedef struct Parameters
+{
+    double *energy;
+    double *magnetitazion;
+} Parameters;
+int xp[L],xn[L],yp[L],yn[L];
+char s[V];
+char s_inicial;
+char s_final;
+int E_i;
+int E_f;
+int n;
+double beta;
+int Naceptados;
+int Ind;
+double *media; double * varianza; double * desviacionMedia;
+void random_config (int *s);
+void toro(int *xp, int *xn, int *yp, int *yn);
+void up_config (int *s);
+void down_config (int *s);
+double energy (int *s, int *xp, int *yp);
+double magnetization (int *s);
+void saveConfiguration(int *s);
+void sweep(int n, double x, double y);
+int sum[5] = {4, -4, 2, -2, 0};
+int dif[5]= {8,4,0,-4,-8};
+double Prob[5]; int i;
+void random_config (int *s);
+void toro(int *xp, int *xn, int *yp, int *yn);
+void up_config (int *s);
+void down_config (int *s);
+double energy (int *s, int *xp, int *yp);
+double magnetization (int *s);
+void saveConfiguration(int *s);
+void sweep(int n, double x, double y);
 
 //configuracion.c functions
 void rand_config(int *s);
